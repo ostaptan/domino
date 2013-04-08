33c91dda2937870e19a4e935b4268f2c33fe76ab
Ostap256 <otan256@gmail.com>
2013-04-09 00:05:21 +0300

    game create & join

--- db/migrate/20130327211675_create_games.rb
+++ db/migrate/20130327211675_create_games.rb
@@ -1,11 +1,16 @@
 class CreateGames < ActiveRecord::Migration
   def change
-    drop_table :games
+    #drop_table :games
     create_table :games do |t|
       t.text :bones
       t.string :game_type, nil => false
       t.integer :time_per_move, :default => 1
       t.integer :winner_id
+      t.integer :players_count, :default => 2
+      t.datetime :finished_at
+      t.datetime :started_at
+      t.integer :min_rating, :default => 10
+      t.integer :max_rating, :default => 10
 
       t.timestamps
     end
